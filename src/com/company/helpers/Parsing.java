9613783eb43f74a3c096e6751018cd0b7f94e54b
Mikhail <ashcheulov_git@fastmail.com>
2020-09-26 18:54:34 +0300

    Revert "Revert "Version 1.1""
    This reverts commit f2d9c75098dc1d4fbd5a206691bc52cdee8e9b06.

--- src/com/company/helpers/Parsing.java
+++ src/com/company/helpers/Parsing.java
@@ -1,10 +1,10 @@
 package com.company.helpers;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 /**
  * Класс для парсинга текста
@@ -13,22 +13,75 @@ public class Parsing {
 
     private Map<String, Integer> metaData = new HashMap<>();
 
+    public List<String> parsList(List<String> list) {
+        List<String> parsedList = new ArrayList<>();
+        for (String line : list) {
+            parsedList.addAll(parsLine(line));
+        }
+        parsedList.removeAll(Collections.singleton(""));
+        return parsedList;
+    }
+
     /**
      * Парсит на слова BufferedReader
      *
      * @param input BufferedReader
      * @return список слов BufferedReader'а
      */
-    public List<String> parsBufferedReader(BufferedReader input) {
-        List<String> lineList = input.lines().collect(Collectors.toList());
+//    public List<String> parsBufferedReader(BufferedReader input) {
+//        List<String> lineList = input.lines().collect(Collectors.toList());
+//        List<String> parsedBufferedReader = new ArrayList<>();
+//        for (String line : lineList) {
+//            parsedBufferedReader.addAll(parsLine(line));
+//        }
+//        parsedBufferedReader.removeAll(Collections.singleton(""));
+//        return parsedBufferedReader;
+//    }
+    public List<String> parsBufferedReader(BufferedReader input) throws IOException {
         List<String> parsedBufferedReader = new ArrayList<>();
-
-        for (String line : lineList) {
-            parsedBufferedReader.addAll(parsLine(line));
+        while (input.ready()) {
+            parsedBufferedReader.addAll(parsLine(input.readLine()));
         }
         parsedBufferedReader.removeAll(Collections.singleton(""));
         return parsedBufferedReader;
     }
+
+//    public void parsBufferedReader(BufferedReader input, BufferedWriter output, Processing processing) throws IOException {
+//        List<String> lineList = new ArrayList<>();
+//        List<File> files = new ArrayList<>();
+//        while (input.ready()) {
+//            Instant starts = Instant.now();
+//            for (int i = 0; i < 100000; i++) {
+//                if (input.ready())
+//                    lineList.add(input.readLine());
+//                else break;
+//            }
+//            System.out.println("0 - "+Duration.between(starts, Instant.now()));
+//            files.add(new File(String.format("data%s.txt", files.size())));
+//            threads.add(new WordsThread());
+//            threads.get(threads.size()-1).setLineList(lineList,files.get(files.size()-1),this);
+//            threads.get(threads.size()-1).start();
+//            System.out.println("1 - "+Duration.between(starts, Instant.now()));
+//            lineList.clear();
+//        }
+//
+//        for(WordsThread thread:threads) {
+//            while (thread.isAlive()) {
+//            }
+//        }
+//
+//        for(File file:files) {
+//            BufferedReader inp = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+//            while (inp.ready()) {
+//                processing.form(inp.readLine());
+//            }
+//        }
+//
+//        for (Map.Entry<String, Integer> i : processing.sort(false)) {
+//            output.write(String.format("%s : %s\n", i.getKey(), i.getValue()));
+//            output.flush();
+//        }
+//    }
 
     /**
      * Парсит линию по regexList
@@ -41,8 +94,8 @@ public class Parsing {
         String delimiter = "&nwrd";
         for (String regex : regexList) {
             Pattern pattern = Pattern.compile(regex);
-            Matcher matcher = pattern.matcher(line);
-            count(matcher);
+//            Matcher matcher = pattern.matcher(line);
+//            count(matcher);
             line = String.join(delimiter, pattern.split(line));
         }
         return Arrays.asList(Pattern.compile(delimiter).split(line));
@@ -78,5 +131,16 @@ public class Parsing {
      */
     public Map<String, Integer> getMetaData() {
         return metaData;
+    }
+
+    public Processing parsBufferedReaderAndDoProcessing(BufferedReader input, Processing processing) throws IOException {
+        while (input.ready()) {
+            for (String word : parsLine(input.readLine())) {
+                if (!word.equals("")) {
+                    processing.form(word);
+                }
+            }
+        }
+        return processing;
     }
 }
