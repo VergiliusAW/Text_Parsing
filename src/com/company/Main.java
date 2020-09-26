f2d9c75098dc1d4fbd5a206691bc52cdee8e9b06
Mikhail <ashcheulov_git@fastmail.com>
2020-09-26 18:50:20 +0300

    Revert "Version 1.1"
    This reverts commit 308b2c899a216c65f648354d51aac536f87d9981.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,37 +4,28 @@ import com.company.helpers.Parsing;
 import com.company.helpers.Processing;
 
 import java.io.*;
-import java.time.Duration;
-import java.time.Instant;
 import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        Instant starts = Instant.now();
-        String inputFileName = "textL.txt";
+        String inputFileName = "input.txt";
         String outputFileName = "output.txt";
         BufferedReader input = new BufferedReader(new InputStreamReader(new FileInputStream(inputFileName)));
         BufferedWriter output = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputFileName)));
         Processing processing = new Processing();
         Parsing parsing = new Parsing();
         try {
-//            parsing.parsBufferedReader(input,output,processing);
-//            for (Object i : parsing.parsBufferedReader(input)) {
-//                processing.form((String) i);
-//            }
-            for (Map.Entry<String, Integer> i : parsing.parsBufferedReaderAndDoProcessing(input, processing).sort(false)) {
+            for (Object i : parsing.parsBufferedReader(input)) {
+                processing.form((String) i);
+            }
+//            Parsing.getMetaData().forEach((s, integer) -> System.out.println(s+" : "+integer));
+            for (Map.Entry<String, Integer> i : processing.sort(false)) {
                 output.write(String.format("%s : %s\n", i.getKey(), i.getValue()));
                 output.flush();
             }
-//            parsing.getMetaData().forEach((s, integer) -> System.out.println(s+" : "+integer));
-//            for (Map.Entry<String, Integer> i : processing.sort(false)) {
-//                output.write(String.format("%s : %s\n", i.getKey(), i.getValue()));
-//                output.flush();
-//            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(Duration.between(starts, Instant.now()));
     }
 }
